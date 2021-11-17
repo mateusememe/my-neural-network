25c9452c93c0b848767829bf35e77affad370d09
Santanicius <santanaramosvinicius@gmail.com>
2021-11-17 11:47:09 -0300

    update #1

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,15 +2,28 @@ import numpy as np
 import pandas as pd
 
 
+
 class NeuralNetwork:
-    def __init__(self) -> None:
+    def __init__(self):
         hiddenLayer = []
         inputs = []
         outputLayer = []
         errorArray = []
+        mode = 'linear'
     
-    def __linear(self):
-        return 1/10
+    def propag(self, net):
+        if self.mode == "linear":
+            return net/10
+        if self.mode == "logistic":
+            return 1 / (1 + np.exp(-net))
+        if self.mode == "hyperbolic":
+            return (1 - np.exp(-2*net)) / (1 + np.exp(-2*net))
     
-    def logistic():
-        return 1
+
+    def gradient(self, v):  
+        if self.mode == "linear":
+            return 0.1
+        if self.mode == "logistic":
+            return propag(v)*(1-propag(v))
+        if self.mode == "hyperbolic":
+            return 1 - propag(v)**2
